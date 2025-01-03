345c39e50b8e948a13498697837cbc44588d0ab4
Minuke <nakamadeath@gmail.com>
2025-01-03 13:44:19 +0100

    refactor product client service to implement resource-based product fetching

--- src/app/features/products/services/product-client.service.ts
+++ src/app/features/products/services/product-client.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, resource, WritableResource } from '@angular/core';
 import { Product } from '@entities/interfaces/product.interface';
 import { Observable } from 'rxjs';
 
@@ -11,8 +11,21 @@ export class ProductClientService {
   private readonly API_URL = "https://dummyjson.com/products";
   private http: HttpClient = inject(HttpClient);
 
-  public getProducts(): Observable<{products: Product[]}> {
-    return this.http.get<{products: Product[]}>(this.API_URL);
+  // public getProducts(): Observable<{products: Product[]}> {
+  //   return this.http.get<{products: Product[]}>(this.API_URL);
+  // }
+
+  public productsResource: WritableResource<Product[]> = resource({
+    loader: async () => {
+      const response = await fetch(this.API_URL);
+      if (!response.ok) throw new Error('Failed to fetch products');
+      return response.json();
+    },
+  });
+
+  public getProducts() {
+    return this.productsResource;
   }
+
   
 }
